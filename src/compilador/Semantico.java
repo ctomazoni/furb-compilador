7df6864d74bb0b49dca29ce5eb4d893e386acdaf
Cleber Tomazoni <32346186+ctomazoni@users.noreply.github.com>
2018-06-26 18:40:29 -0300

    Adicionadas as ações #7 a #13

--- src/compilador/Semantico.java
+++ src/compilador/Semantico.java
@@ -13,17 +13,17 @@ public class Semantico implements Constants {
     public void executeAction(int action, Token token) throws SemanticError {
 
         switch (action) {
-             case 1:
-                executarAcaoSemantica1(token);
+            case 1:
+                executarAcaoSemantica1();
                 break;
             case 2:
-                executarAcaoSemantica2(token);
+                executarAcaoSemantica2();
                 break;
             case 3:
-                executarAcaoSemantica3(token);
+                executarAcaoSemantica3();
                 break;
             case 4:
-                executarAcaoSemantica4(token);
+                executarAcaoSemantica4();
                 break;
             case 5:
                 executarAcaoSemantica5(token);
@@ -31,23 +31,44 @@ public class Semantico implements Constants {
             case 6:
                 executarAcaoSemantica6(token);
                 break;
+            case 7:
+                executarAcaoSemantica7(token);
+                break;
+            case 8:
+                executarAcaoSemantica8(token);
+                break;
+            case 9:
+                executarAcaoSemantica9(token);
+                break;
+            case 10:
+                executarAcaoSemantica10(token);
+                break;
+            case 11:
+                executarAcaoSemantica11(token);
+                break;
+            case 12:
+                executarAcaoSemantica12(token);
+                break;
+            case 13:
+                executarAcaoSemantica13(token);
+                break;
             case 14:
-                executarAcaoSemantica14(token);
+                executarAcaoSemantica14();
                 break;
             case 15:
-                executarAcaoSemantica15(token);
+                executarAcaoSemantica15();
                 break;
             case 16:
-                executarAcaoSemantica16(token);
+                executarAcaoSemantica16();
                 break;
             case 17:
-                executarAcaoSemantica17(token);
+                executarAcaoSemantica17();
                 break;
             case 18:
-                executarAcaoSemantica18(token);
+                executarAcaoSemantica18();
                 break;
             case 19:
-                executarAcaoSemantica19(token);
+                executarAcaoSemantica19();
                 break;
             case 20:
                 executarAcaoSemantica20(token);
@@ -56,7 +77,7 @@ public class Semantico implements Constants {
 
     }
 
-     private void executarAcaoSemantica1(Token token) {
+    private void executarAcaoSemantica1() {
         Tipo tipo1 = pilhaTipos.pop();
         Tipo tipo2 = pilhaTipos.pop();
         if(tipo1 == Tipo.float64 || tipo2 == Tipo.float64){
@@ -66,7 +87,8 @@ public class Semantico implements Constants {
         }
         codigo.append(TAB).append("add").append(QUEBRA_LINHA);
     }
-    private void executarAcaoSemantica2(Token token) {
+    
+    private void executarAcaoSemantica2() {
         Tipo tipo1 = pilhaTipos.pop();
         Tipo tipo2 = pilhaTipos.pop();
         if(tipo1 == Tipo.float64 || tipo2 == Tipo.float64){
@@ -76,7 +98,8 @@ public class Semantico implements Constants {
         }
         codigo.append(TAB).append("sub").append(QUEBRA_LINHA);
     }
-    private void executarAcaoSemantica3(Token token) {
+    
+    private void executarAcaoSemantica3() {
         Tipo tipo1 = pilhaTipos.pop();
         Tipo tipo2 = pilhaTipos.pop();
         if(tipo1 == Tipo.float64 || tipo2 == Tipo.float64){
@@ -86,7 +109,8 @@ public class Semantico implements Constants {
         }
         codigo.append(TAB).append("mul").append(QUEBRA_LINHA);
     }
-    private void executarAcaoSemantica4(Token token) throws SemanticError {
+    
+    private void executarAcaoSemantica4() throws SemanticError {
         Tipo tipo1 = pilhaTipos.pop();
         Tipo tipo2 = pilhaTipos.pop();
         if(tipo1 == tipo2){
@@ -96,18 +120,82 @@ public class Semantico implements Constants {
         }
         codigo.append(TAB).append("div").append(QUEBRA_LINHA);
     }
+    
     private void executarAcaoSemantica5(Token token) {
         pilhaTipos.push(Tipo.int64);
-        codigo.append(TAB).append("ldc.i8 ").append(token.getLexeme()).append(QUEBRA_LINHA);
+        codigo.append(TAB).append("ldc.i8").append(token.getLexeme()).append(QUEBRA_LINHA);
         codigo.append(TAB).append("conv.r8").append(QUEBRA_LINHA);
     }
 
     private void executarAcaoSemantica6(Token token) {
         pilhaTipos.push(Tipo.float64);
-        codigo.append(TAB).append("ldc.r8 ").append(token.getLexeme()).append(QUEBRA_LINHA);
+        codigo.append(TAB).append("ldc.r8").append(token.getLexeme()).append(QUEBRA_LINHA);
     }
 
-    private void executarAcaoSemantica14(Token token) {
+    private void executarAcaoSemantica7(Token token) throws SemanticError {
+        Tipo tipo = pilhaTipos.pop();
+        if (tipo == Tipo.float64
+                || tipo == Tipo.int64) {
+            pilhaTipos.push(tipo);
+        } else {
+            throw new SemanticError("SemanticError#7");
+        }
+    }
+    
+    private void executarAcaoSemantica8(Token token) throws SemanticError {
+        Tipo tipo = pilhaTipos.pop();
+        if (tipo == Tipo.float64
+                || tipo == Tipo.int64) {
+            pilhaTipos.push(tipo);
+        } else {
+            throw new SemanticError("SemanticError#8");
+        }
+        codigo.append(TAB).append("ldc.i8 -1").append(QUEBRA_LINHA);
+        if (tipo == Tipo.int64) {
+            codigo.append(TAB).append("conv.r8").append(QUEBRA_LINHA);
+        }
+        codigo.append(TAB).append("mul").append(QUEBRA_LINHA);
+    }
+    
+    private void executarAcaoSemantica9(Token token) {
+        operador = token.getLexeme();
+    }
+    
+    private void executarAcaoSemantica10(Token token) throws SemanticError {
+        Tipo tipo1 = pilhaTipos.pop();
+        Tipo tipo2 = pilhaTipos.pop();
+        if (tipo1 == tipo2) {
+            pilhaTipos.push(Tipo.bool);
+        } else {
+            throw new SemanticError("SemanticError#9");
+        }
+        switch (operador) {
+            case ">":
+                codigo.append(TAB).append("cgt").append(QUEBRA_LINHA);
+            case "<":
+                codigo.append(TAB).append("clt").append(QUEBRA_LINHA);
+            case "=":
+                codigo.append(TAB).append("ceq").append(QUEBRA_LINHA);
+        }
+    }
+    
+    private void executarAcaoSemantica11(Token token) {
+        pilhaTipos.push(Tipo.bool);
+        codigo.append(TAB).append("ldc.i4.1").append(QUEBRA_LINHA);
+    }
+    
+    private void executarAcaoSemantica12(Token token) {
+        pilhaTipos.push(Tipo.bool);
+        codigo.append(TAB).append("ldc.i4.0").append(QUEBRA_LINHA);
+    }
+    
+    private void executarAcaoSemantica13(Token token) {
+        pilhaTipos.push(Tipo.int64);
+        codigo.append("ldc.i8").append(token.getLexeme()).append(QUEBRA_LINHA);
+        codigo.append("conv.r8").append(QUEBRA_LINHA);
+    }
+    
+    private void executarAcaoSemantica14() {
         Tipo tipo = pilhaTipos.pop();
         if (tipo == Tipo.int64) {
             codigo.append(TAB).append("conv.i8").append(QUEBRA_LINHA);
@@ -115,37 +203,37 @@ public class Semantico implements Constants {
         codigo.append(TAB).append("call void [mscorlib]System.Console::Write(").append(tipo.name()).append(")").append(QUEBRA_LINHA);
     }
 
-    private void executarAcaoSemantica15(Token token) {
+    private void executarAcaoSemantica15() {
         codigo.append(".assembly extern mscorlib {}").append(QUEBRA_LINHA);
-        codigo.append(".assembly _codigo_objeto{} ").append(QUEBRA_LINHA);
+        codigo.append(".assembly _codigo_objeto{}").append(QUEBRA_LINHA);
         codigo.append(".module   _codigo_objeto.exe").append(QUEBRA_LINHA).append(QUEBRA_LINHA);
         codigo.append(".class public _UNICA{").append(QUEBRA_LINHA);
         codigo.append(".method static public void _principal() {").append(QUEBRA_LINHA);
-        codigo.append(TAB).append(".entrypoint ").append(QUEBRA_LINHA);
+        codigo.append(TAB).append(".entrypoint").append(QUEBRA_LINHA);
     }
 
-    private void executarAcaoSemantica16(Token token) {
-        codigo.append(TAB).append("ret ").append(QUEBRA_LINHA);
-        codigo.append(TAB).append("} ").append(QUEBRA_LINHA);
-        codigo.append("} ").append(QUEBRA_LINHA);
+    private void executarAcaoSemantica16() {
+        codigo.append(TAB).append("ret").append(QUEBRA_LINHA);
+        codigo.append(TAB).append("}").append(QUEBRA_LINHA);
+        codigo.append("}").append(QUEBRA_LINHA);
     }
 
-    private void executarAcaoSemantica17(Token token) {
+    private void executarAcaoSemantica17() {
         codigo.append(TAB).append("ldstr \"\\n\"").append(QUEBRA_LINHA);
         codigo.append(TAB).append("call void [mscorlib]System.Console::Write(string)").append(QUEBRA_LINHA);
     }
 
-    private void executarAcaoSemantica18(Token token) {
+    private void executarAcaoSemantica18() {
         codigo.append(TAB).append("and").append(QUEBRA_LINHA);
     }
 
-    private void executarAcaoSemantica19(Token token) {
+    private void executarAcaoSemantica19() {
         codigo.append(TAB).append("or").append(QUEBRA_LINHA);
     }
 
     private void executarAcaoSemantica20(Token token) {
         pilhaTipos.push(Tipo.string);
-        codigo.append(TAB).append("ldstr ").append(token.getLexeme()).append(QUEBRA_LINHA);
+        codigo.append(TAB).append("ldstr").append(token.getLexeme()).append(QUEBRA_LINHA);
     }
 
     public String getCodigo() {
